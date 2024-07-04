1fb91e7fe5695198a53cf8635d22d340806a19f4
Inju Michorius <inju@hotmail.nl>
2024-07-04 23:28:54 +0200

    Add styling for leaderboard

--- src/components/organisms/navBar.js
+++ src/components/organisms/navBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Hamburger from "../atoms/hamburger";
+import avatar from "../../assets/images/character1.png";
 
 function NavBar() {
   const [isPlayerListVisible, setIsPlayerListVisible] = useState(false);
@@ -12,9 +13,19 @@ function NavBar() {
     <header className="navbar-container">
       <Hamburger onClick={handleHamburgerClick} />
       <ul className={`player-list ${isPlayerListVisible ? "show" : ""}`}>
-        <h1>Leaderboard</h1>
+        <h1 className="player-list__title">Leaderboard</h1>
         <li className="player-container">
-          <div className="avatar-preview">avatar</div>
+          <div className="avatar-preview">
+            <img src={avatar} alt="Avatar" />
+          </div>
+          <div className="user-details">
+            <p className="player-name">Player 1</p>
+            <div className="items-container">
+              <div className="item">1</div>
+              <div className="item">2</div>
+              <div className="item">3</div>
+            </div>
+          </div>
           <div className="place">1</div>
         </li>
       </ul>
